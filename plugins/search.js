60ab4a9ecacf74d1de97b0f96aff5189a1ce0e30
K.T <z1022001@gmail.com>
2024-03-13 11:48:18 +0800

    search add slash cmd

--- plugins/search.js
+++ plugins/search.js
@@ -1,86 +1,167 @@
-const { EmbedBuilder } = require("@discordjs/builders");
+const { EmbedBuilder, ApplicationCommandOptionType, MessagePayload } = require('discord.js');
 
 const getTimeFromDiscordSnowflake = (snowflake) => (Number(BigInt(snowflake) >> 22n) + 14200704e5);
+
+const searchKeyword = async (client, guild, keyword, days = 0) => {
+
+    days = Math.min(7, days);   // set days max limit
+
+    // get channels
+    const channels = await guild.channels.fetch();
+
+    // set time limit
+    const timenow = Date.now() - 3000;
+    const timelimit = Date.now() - ((days * 86400000) || 28800000);  // search msg in 8 hrs or n days
+    let result = new Map();
+    const resultLimit = 20;
+
+    // search in channels
+    for (const [cID] of channels) {
+
+        // get channel
+        const channel = await client.channels.fetch(cID);
+
+        // skip invalid channel
+        if (!channel?.lastMessageId || ![0, 10, 11, 12, 14, 15, 16].includes(channel.type)) { continue; }
+        // skip old channel
+        if (timelimit > getTimeFromDiscordSnowflake(channel.lastMessageId)) { continue; }
+
+        // console.log(`<#${cID}>`, channel.name);
+        let before;
+        while (1) {
+
+            // get messages
+            const msgs = await channel.messages.fetch({ before, force: true });
+            const keys = Array.from(msgs.keys());
+
+            for (const mID of keys) {
+                const msg = msgs.get(mID);
+                before = mID;
+                // console.log(`<-${mID}>`, (msg.content?.split('\n') || [])[0]?.trim());
+
+                // check message timestamp
+                if (timelimit > msg.createdTimestamp) { before = -1; break; }   // skip old message
+                if (msg.createdTimestamp > timenow) { continue; }               // skip new message
+
+                // found in content
+                if (msg.content) {
+                    const { content } = msg;
+
+                    if (content.toLowerCase().includes(keyword.toLowerCase())) {
+                        result.set(msg.id, msg);
+                        continue;
+                    }
+                }
+                // found in embeds
+                if (Array.isArray(msg.embeds)) {
+                    for (const embed of msg.embeds) {
+                        const content = `${embed.title} ${embed.url} ${embed.description}` || '';
+
+                        if (content.toLowerCase().includes(keyword.toLowerCase())) {
+                            result.set(msg.id, msg);
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+            if (before == -1) { break; }                // message too old, to next channel
+            if (keys.length == 0) { break; }            // message not found, to next channel
+        }
+    }
+
+    let description = [];
+    for (const key of Array.from(result.keys()).sort().reverse()) {
+        const msg = result.get(key);
+
+        description.push(msg.url);
+
+        if (description.length > resultLimit) {
+            description.push(`... and more ${result.size - resultLimit} results ...`);
+            break;
+        }
+    }
+    if (description.length == 0) { description = ['Not found.']; }
+
+    let embed = new EmbedBuilder()
+        .setTitle(`Search: ${keyword}`)
+        .setDescription(description.join('\n'));
+
+    // let fields = [];
+    // fields.push({ name: `Search:`, value: keyword });
+    // fields.push({ name: `Result:`, value: result.length });
+    // fields.push({ name: `Position`, value: description.join('\n') });
+    // let embed = new EmbedBuilder().addFields(fields);
+
+    return { embeds: [embed], allowedMentions: { repliedUser: false }, ephemeral: true };
+}
 
 module.exports = {
     name: 'search',
     description: "search string in guild",
+
+    async setup(client) {
+
+        await client.application.commands.fetch();
+
+        // // delete all slash command
+        // for (let [key, value] of client.application.commands.cache) {
+        //     await value.delete();
+        // }
+
+        // registre slash command
+        if (!client.application.commands.cache.find(c => c.name == 'search')) {
+            client.application.commands.create({
+                name: 'search', description: '搜尋',
+                options: [{
+                    name: 'keyeord', description: "關鍵字",
+                    type: ApplicationCommandOptionType.String, required: true,
+                }, {
+                    name: 'days', description: "時間範圍(日)",
+                    type: ApplicationCommandOptionType.Integer, required: false,
+                }]
+            });
+        }
+    },
+
+
+
+    async interactionCreate(interaction, pluginConfig) {
+        // command type
+        if (!interaction.isCommand()) { return false; }
+        if (interaction.commandName != 'search') { return false; }
+
+        // keep reply postion
+        interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Searching...`)], allowedMentions: { repliedUser: false }, ephemeral: true })
+            .catch((e) => console.log(e.message));
+
+        // start search
+        const { client, guild } = interaction;
+        let keyword = interaction.options.data[0].value;
+        let days = interaction.options.data[1].value;
+        const messagePayload = await searchKeyword(client, guild, keyword, days);
+
+        // reply result
+        interaction.editReply(messagePayload).catch((e) => console.log(e.message));
+
+        return true;
+    },
 
     async execute(message, pluginConfig, command, args, lines) {
         const { client, guild } = message;
 
         if (command == 'search') {
 
+            // keep reply postion
+            let reply = await message.reply({ embeds: [new EmbedBuilder().setDescription(`Searching...`)], allowedMentions: { repliedUser: false } })
+                .catch((e) => console.log(e.message));
 
-            let reply = await message.reply({
-                embeds: [new EmbedBuilder().setDescription(`Searching...`)], allowedMentions: { repliedUser: false }
-            }).catch(() => { });
+            // start search
+            const messagePayload = await searchKeyword(client, guild, keyword);
 
-            const channels = await guild.channels.fetch();
-
-            // set time limit
-            const now = Date.now() - 28800000;  // search msg in 8 hrs
-            let result = [];
-
-            for (const [cID] of channels) {
-                const channel = await client.channels.fetch(cID);
-
-                let before;
-                while (1) {
-                    // console.log(`<#${cID}>`, channel.name);
-
-                    if (!channel?.lastMessageId || ![0, 10, 11, 12, 14, 15, 16].includes(channel.type)) { break; }
-                    if (now > getTimeFromDiscordSnowflake(channel.lastMessageId)) { break; }
-
-                    const msgs = await channel.messages.fetch({ before, force: true });
-                    const keys = Array.from(msgs.keys());
-
-                    for (const mID of keys) {
-                        const msg = msgs.get(mID);
-                        before = mID;
-
-                        if (msg.id == message.id) { continue; }
-
-                        // check time limit
-                        if (now > msg.createdTimestamp) { before = -1; break; }
-
-                        // found
-                        if (msg.content.includes(args[0]) ||
-                            msg.embeds && Array.isArray(msg.embeds) && msg.embeds[0]?.description
-                            && msg.embeds[0].description.includes(args[0])) {
-
-                            result.push(msg);
-                            continue;
-                        }
-                    }
-
-                    if (before == -1 || keys.length == 0) { break; }
-                }
-            }
-
-            let description = [];
-            for (let i = 0; (i < 20 && i < result.length); ++i) {
-                description.push(result[i].url);
-            }
-            if (result.length > 20) {
-                description.push(`... and more ${result.length - 20} result ...`);
-            }
-            if (description.length == 0) {
-                description = ['Not found.'];
-            }
-
-            let embed = new EmbedBuilder()
-                .setTitle(`Search: ${args[0]}`)
-                .setDescription(description.join('\n'));
-
-            // let fields = [];
-            // fields.push({ name: `Search:`, value: args[0] });
-            // fields.push({ name: `Result:`, value: result.length });
-            // fields.push({ name: `Position`, value: description.join('\n') });
-            // let embed = new EmbedBuilder().addFields(fields);
-
-            reply.edit({ embeds: [embed], allowedMentions: { repliedUser: false } })
-                .catch(() => { });
+            // reply result
+            reply.edit(messagePayload).catch((e) => console.log(e.message));
         }
 
         return;
