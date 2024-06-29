41a279151e81989183dbf21af371c266f66fb580
K.T. <z1022001@gmail.com>
2024-06-29 19:30:13 +0800

    mc4 fix cookie from Private browsing

--- configs/DICE/713622845682614302.js
+++ configs/DICE/713622845682614302.js
@@ -8,12 +8,12 @@ module.exports = {      // KTG
         {
             holoChannelID: 'UCUKD-uaobj9jiqB-VXt71mA',   // SSRB
 
-            streamChannelID: '1024627739023650827',      // #_stream
-            memberChannelID: '1024627744681771108',      // #_member
+            streamChannelID: '1024627739023650827',      // #⚫_stream <#1024627739023650827>
+            memberChannelID: '1024627744681771108',      // #⚫_member <#1024627744681771108>
 
             expiresKey: 'ssrb_expires',
-            logChannelID: '1024627281592848434',         // #⁠_log
-            memberRoleID: '1009001004454383656',          // @&TEST1
+            logChannelID: '1024627281592848434',         // #_log <#1024627281592848434>
+            memberRoleID: '1009001004454383656',          // TEST1 <@&1009001004454383656>
             // memberLevelID: [
             //     `1150843451726905375`, `1150843463420612800`, `1150843475324047401`, `1150843481095413760`,
             //     `1150843488125059143`, `1150843493254713404`, `1150843498598236240`, `1150843504738713762`
@@ -23,23 +23,23 @@ module.exports = {      // KTG
         {
             holoChannelID: 'UC_vMYWcDjmfdpH6r4TTn1MQ',   // kazama,
 
-            streamChannelID: '1236925125434146816',      // #_stream2
-            memberChannelID: '1236925146674106441',      // #_member2
+            streamChannelID: '1236925125434146816',      // #⚫_stream2 <#1236925125434146816>
+            memberChannelID: '1236925146674106441',      // #⚫_member2 <#1236925146674106441>
 
             expiresKey: 'kzmi_expires',
-            logChannelID: '1024627281592848434',         // #⁠_log
-            memberRoleID: '1110090223821520906',          // @&TEST2
+            logChannelID: '1024627281592848434',         // #_log <#1024627281592848434>
+            memberRoleID: '1110090223821520906',          // TEST2 <@&1110090223821520906>
         },//*/
         /*
         {
             holoChannelID: 'UCWQtYtq9EOB4-I5P-3fh8lA',   // otonose,
 
-            streamChannelID: '1024627739023650827',      // #_stream
-            memberChannelID: '1024627744681771108',      // #_member
+            streamChannelID: '1024627739023650827',      // #⚫_stream <#1024627739023650827>
+            memberChannelID: '1024627744681771108',      // #⚫_member <#1024627744681771108>
 
             expiresKey: 'otos_expires',
-            logChannelID: '1024627281592848434',         // #⁠_log
-            memberRoleID: '1179351663925022850',          // @&TEST3
+            logChannelID: '1024627281592848434',         // #_log <#1024627281592848434>
+            memberRoleID: '1179351663925022850',          // TEST3 <@&1179351663925022850>
         },//*/
     ],
 
@@ -47,7 +47,7 @@ module.exports = {      // KTG
     // twitterListener3: [
     //     // #dl
     //     {
-    //         RETWEET_CHANNEL_ID: '1169461312657567765', RETWEET_KEYWORD: [
+    //         RETWEET_CHANNEL_ID: '1169461312657567765', RETWEET_KEYWORD: [    // #dlimg <#1169461312657567765>
     //             '(from:AtedayoAigis) until:2021-03-18 filter:links'
     //         ],
     //     },
@@ -56,23 +56,23 @@ module.exports = {      // KTG
 
 
     reportThread: {
-        REPORT_CHANNEL_ID: '1024627536862400582',      // #_report
-        ADMIN_ROLE_ID: '881775866835763240',      // @&==BOT==
+        REPORT_CHANNEL_ID: '1024627536862400582',      // #_report <#1024627536862400582>
+        ADMIN_ROLE_ID: '881775866835763240',      // ==BOT== <@&881775866835763240>
     },
 
     // /*
     spamBotKicker: {
-        LOG_CHANNEL_ID: '1024627281592848434',      // #_log
-        PERMISSION_ROLE_ID: '1009001004454383656',      // @&TEST
+        LOG_CHANNEL_ID: '1024627281592848434',      // #_log <#1024627281592848434>
+        PERMISSION_ROLE_ID: '1009001004454383656',      // TEST1 <@&1009001004454383656>
         // PERMISSION_ROLE_ID: null,
-        BAN_CHANNEL_ID: '1024627481111691275'      // #_ban
+        BAN_CHANNEL_ID: '1024627481111691275'      // #_ban <#1024627481111691275>
     },//*/
 
 
 
-    messageLogger: { LOG_CHANNEL_ID: '1024627281592848434' },      // #_bot-test
-    memberCounter: { COUNTER_CHANNEL_ID: '1024628239865491486' },      // #群組人數: 4,840
-    reactionRole: { RULE_CHANNEL_ID: '1024628143232925706' },      // #_role
+    messageLogger: { LOG_CHANNEL_ID: '1024627281592848434' },      // #_log <#1024627281592848434>
+    memberCounter: { COUNTER_CHANNEL_ID: '1024628239865491486' },      // #群組人數-15 <#1024628239865491486>
+    reactionRole: { RULE_CHANNEL_ID: '1024628143232925706' },      // #_rule <#1024628143232925706>
 
     delall: {},
     dlimg: {},
