1b097487ff80200e1182b562f691285a7266e931
Yinan <z1022001@gmail.com>
2024-12-22 20:44:05 +0800

    chibiko

--- configs/DLSITE/config.js
+++ configs/DLSITE/config.js
@@ -42,7 +42,7 @@ module.exports = {
         'twitterAntiFilter',
         'twitterListener3',
         // 'uptimer',
-        // 'verifyThread',
+        'verifyThread',
         // 'welcomeMsg',
     ],
 
