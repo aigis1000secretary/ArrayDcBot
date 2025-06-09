2dbf4859d99b19e146cfd2033a61c5a8b9310aa3
Yinan <z1022001@gmail.com>
2025-06-09 11:50:03 +0800

    http server

---
+++ index.js
@@ -0,0 +1,84 @@
+
+const server = require('./server.js');
+
+require('dotenv').config();
+
+const fs = require('fs');
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const clients = [];
+module.exports.terminate = async (force = false) => {
+    // check all clients
+    const busy = [];
+    for (const client of clients) {
+        // check all plugins
+        for (const [key, value] of client.commands) {
+            // idleCheck method
+            const idle = typeof (value?.idleCheck) == "function" ? value.idleCheck() : null;
+
+            if (idle) { continue; }
+
+            busy.push(`${client.mainConfig.botName}:${key}`);
+        }
+    }
+
+    // log logs
+    if (busy.length > 0 && !force) { console.log(`[${busy.join(', ')}] is busy, terminate fail.`); return; }
+
+    // emit all client
+    for (const client of clients) {
+        client.emit('close');
+    }
+
+    // terminate http server
+    server.terminate();
+
+    await sleep(1500);
+
+    // shutdown app
+    process.exit(1);
+};
+
+module.exports.getBotIDs = () => {
+    let botIDs = [];
+    for (const client of clients) {
+        botIDs.push(client.user.id);
+    }
+    return botIDs;
+};
+
+(async () => {
+
+    // defind host
+    if (process.env.HOST_TYPE == 'HEROKU') { process.env.HOST_URL = 'https://arraydcbot.herokuapp.com'; }
+    if (process.env.HOST_TYPE == 'FLY_IO') { process.env.HOST_URL = 'https://arraydcbot.fly.dev'; }
+    if (process.env.HOST_TYPE == 'debug') { process.env.HOST_URL = 'https://2897-114-36-103-113.ngrok-free.app'; }
+
+    // web server
+    // process.env.PORT = 3001;    // for dice debug
+    server.init();
+
+    // for (const bot of [
+    //     'DLSITE',
+    //     'SSRB', 'POKOBE', 'OTOSE', 'BANPEN', 'RIRIKA', 'AOKU',  /*
+    //     'DICE'   //*/
+    // ]) {
+    //     const configPath = `./configs/${bot}/`;
+
+    //     if (fs.existsSync(`./bot.js`)) {
+    //         let botJs = require(`./bot.js`);
+
+    //         let client = await botJs.init(configPath);
+    //         if (client) { clients.push(client); }
+    //     }
+    // }
+
+    // // load modules
+    // const modulesFiles = fs.readdirSync(`./modules/`)
+    //     .filter(file => file.endsWith('.js'));
+
+    // for (const file of modulesFiles) {
+    //     require(`./modules/${file}`);
+    // }
+
+})();
---
+++ modules/undici-request.js
@@ -0,0 +1,61 @@
+
+// const { request: _request } = require('undici');
+const queryString = require('qs');
+
+const _request = async function () {
+    throw new Error('test error');
+    return null;
+}
+
+module.exports = {
+    request: async function ({ url, qs, method = 'GET', headers = {}, body }) {
+        console.log(method, url);
+
+        const uri = (!!qs ? new URL(url + '?' + queryString.stringify(qs)) : new URL(url)).toString();
+        const options = { method, headers, body };
+
+        const result = { statusCode: null, body: null, text: null, error: null };
+
+        let response;
+        try {
+            response = await _request(uri, options);
+        } catch (e) {
+            result.error = e;
+            return result;
+        }
+
+        const contentType = response.headers['content-type'] || '';
+        const _text = await response.body.text();
+        result.statusCode = response.statusCode;
+        result.text = _text;
+
+        if (contentType.includes('application/json')) {
+            try {
+                result.body = JSON.parse(_text);
+            } catch (e) {
+                result.body = _text;
+                result.error = new Error('Failed to parse JSON');
+            }
+        } else {
+            result.body = _text;
+        }
+
+        return result;
+    },
+
+    get: function ({ url, qs, headers }) {
+        return module.exports.request({ url, qs, method: 'GET', headers });
+    },
+
+    post: function ({ url, qs, form, headers = {} }) {
+        const body = new URLSearchParams(form);
+        headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        return module.exports.request({ url, qs, method: 'POST', headers, body });
+    },
+
+    delete: function ({ url, qs, headers }) {
+        return module.exports.request({ url, qs, method: 'DELETE', headers });
+    },
+    // pipe: require('util').promisify(require('stream').pipeline),
+
+}
